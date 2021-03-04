e9280b9de32f3bdfbe30736fcea2c1c86458d53e
fibo <casati_gianluca@yahoo.it>
2021-03-04 18:58:46 +0100

    use CommonJS syntax

--- src/index.js
+++ src/index.js
@@ -1,1 +1,41 @@
-export * from './locations.js'
+const { domainName } = require('mycryptos-locations')
+
+const cssBaseUrl = `https://${domainName.assets}/css`
+const fontsBaseUrl = `https://${domainName.assets}/fonts`
+const imagesBaseUrl = `https://${domainName.assets}/images`
+
+const cssFilename = {
+  auth: 'auth.css'
+}
+
+const fontFilename = {
+  aldrichFontFace: 'aldrich.css',
+  aldrichFontFile: 'aldrich-regular.ttf'
+}
+
+const imageFilename = {
+  favicon: 'favicon.ico',
+  logotype: 'logotype.png',
+  logotypeWhite: 'logotype-white.png'
+}
+
+const cssUrl = {
+  auth: () => `${cssBaseUrl}/${cssFilename.auth}`
+}
+
+const fontUrl = {
+  primaryFontFace: () => `${fontsBaseUrl}/${fontFilename.aldrichFontFace}`,
+  primaryFontFile: () => `${fontsBaseUrl}/${fontFilename.aldrichFontFile}`
+}
+
+const imageUrl = {
+  favicon: () => `${imagesBaseUrl}/${imageFilename.favicon}`,
+  logotype: () => `${imagesBaseUrl}/${imageFilename.logotype}`,
+  logotypeWhite: () => `${imagesBaseUrl}/${imageFilename.logotypeWhite}`
+}
+
+module.exports = {
+  cssUrl,
+  fontUrl,
+  imageUrl
+}
